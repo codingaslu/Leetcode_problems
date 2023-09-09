3940b4f13743e0c96ed85d2664b15d8307bd3b28
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-09 20:57:55 +0530

    Time: 1200 ms (41.18%), Space: 31.4 MB (10.48%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.py
@@ -1,23 +1,50 @@
 class Solution:
-    def maxFrequency(self, nums: List[int], k: int) -> int:
-        A= nums
+    def maxFrequency(self, A: List[int], k: int) -> int:
+        # Sort the input list A
         A.sort()
-        i=0
-        j=0
-        N=len(A)
-        summation = 0
-        max_freq = 0
+        i = 0  # Left pointer of the window
+        j = 0  # Right pointer of the window
+        N = len(A)  # Length of the list A
+        summation = 0  # Sum of elements in the current window
+        max_freq = 0  # Maximum frequency found so far
 
+        # Define the invalid condition for the window
         def invalid():
-            return A[j] * (j - i + 1) > summation +k
+            return (j - i + 1) * A[j] - summation > k
 
-        while j<N:
-            summation+=A[j]
+        while j < N:
+            summation += A[j]  # Add the current element to the window sum
             while invalid():
-                summation-=A[i]
-                i+=1
-            max_freq = max(max_freq,j-i+1)
-            j+=1
-        return max_freq
+                summation -= A[i]  # Remove the leftmost element from the window
+                i += 1  # Move the left pointer to the right
+            max_freq = max(max_freq, j - i + 1)  # Update the maximum frequency
+            j += 1  # Move the right pointer to the right
+
+        return max_freq  # Return the maximum frequency
+
+
+        
+
+def maxValidWindow(A, N):
+    i = 0
+    j = 0
+    ans = 0
+    
+    def invalid():
+        # Define your invalid condition here
+        # This function should return True if the window is invalid, False otherwise
+        pass
+
+    while j < N:
+        # CODE: Use A[j] to update state which might make the window invalid
+        if invalid():
+            # When invalid, keep shrinking the left edge until it's valid again
+            # CODE: Update state using A[i]
+            i += 1
+        else:
+            ans = max(ans, j - i + 1)  # The window [i, j] is the maximum window found thus far
+            j += 1
+    
+    return ans
 
 
