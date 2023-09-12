f3184a1ddfc176ce7c8bb17fae3faee5434fbe05
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-13 00:17:59 +0530

    Time: 386 ms (96.26%), Space: 17.5 MB (60.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,12 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums)-1
+        # Initialize the goal as the last index of the input list
+        goal = len(nums) - 1
 
-        for i in range(len(nums)-1,-1,-1):
+        # Iterate through the list in reverse order
+        for i in range(len(nums) - 1, -1, -1):
+            # If we can jump from the current position (i) to the goal or beyond,
+            # update the goal to the current position (i)
             if i + nums[i] >= goal:
-                goal=i
-            
-        return True if goal==0 else False
+                goal = i
 
-
-            
+        # If we reach the beginning of the list (goal is 0), it means we can jump
+        # from the start to the end, so return True. Otherwise, return False.
+        return True if goal == 0 else False
