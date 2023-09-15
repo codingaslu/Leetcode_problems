19ff02534f17b164ddc8009d6c334a88daa08d16
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-15 21:48:09 +0530

    Time: 38 ms (71.98%), Space: 16.1 MB (92.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,16 @@
+#https://www.youtube.com/watch?v=73r3KWiEvyk        
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1=0
-        rob2=0
-        # [rob1,rob2,n,n+1,...]
-        for n in nums:
-            temp=max(n+rob1,rob2)
-            rob1=rob2
-            rob2=temp
+        rob1 = 0    # Initialize the variable to store the maximum amount robbed from the previous house
+        rob2 = 0    # Initialize the variable to store the maximum amount robbed from two houses ago
         
-        return rob2
+        # [rob1,rob2,n,n+1,...]
+        # Iterate through the list of house values
+        for n in nums:
+            temp = max(n + rob1, rob2)  # Calculate the maximum amount that can be robbed if we choose the current house or skip it
+            rob1 = rob2               # Update rob1 to the previous rob2 value (moving it one step ahead)
+            rob2 = temp               # Update rob2 to the newly calculated maximum value
+        
+        return rob2   # Return the maximum amount that can be robbed after iterating through all the houses
+
         
