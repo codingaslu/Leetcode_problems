5f9258cccf1f67f9685cced8718af34ccdaacddf
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-15 19:32:13 +0530

    Time: 43 ms (41.76%), Space: 16.1 MB (99.26%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,12 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        l=0
-        r=0
+        l = 0  # Initialize a pointer 'l' for string 't'.
+        r = 0  # Initialize a pointer 'r' for string 's'.
 
-        for i in range(len(t)):
-            if r == len(s):
+        for i in range(len(t)):  # Iterate through characters in string 't'.
+            if r == len(s):  # If all characters in 's' have been matched, return True.
                 return True
-            if t[l] == s[r]:
-                r+=1
-            l+=1
-        return r == len(s)
+            if t[l] == s[r]:  # If the current character in 't' matches the current character in 's', move 'r' forward.
+                r += 1
+            l += 1  # Move 'l' forward to check the next character in 't'.
+
+        return r == len(s)  # Return True if all characters in 's' have been matched, otherwise, return False.
