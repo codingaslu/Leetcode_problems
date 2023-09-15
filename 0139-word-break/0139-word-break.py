6e558818aad80704c86f2cd7971e9d4164ca9051
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-15 11:16:19 +0530

    Time: 51 ms (25.27%), Space: 16.3 MB (96.5%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,19 +1,29 @@
+# https://www.youtube.com/watch?v=Sx9NNgInc3A&t=650s
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-
-        dp = [False] * (len(s)+1)
-
+        
+        # Create a dynamic programming (DP) array to store subproblem results.
+        dp = [False] * (len(s) + 1)
+        
+        # Initialize the DP array. dp[i] will be True if s[i:] can be segmented.
         dp[len(s)] = True
 
-        for i in range(len(s)-1,-1,-1):
+        # Iterate through the string s in reverse order.
+        for i in range(len(s) - 1, -1, -1):
+            # Iterate through each word in the wordDict.
             for w in wordDict:
+                # Check if it's possible to break the string from the current position (i)
+                # using the word (w) from wordDict.
                 if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
-                    dp[i] = dp[i + len(w)]
+                    dp[i] = dp[i + len(w)]  # Update dp[i] based on subproblem result.
                 
+                # If dp[i] is True (i.e., s[i:] can be segmented), no need to check other words.
                 if dp[i]:
                     break
         
+        # The final result is stored in dp[0], which represents whether the entire string can be segmented.
         return dp[0]
+
             
 
 
