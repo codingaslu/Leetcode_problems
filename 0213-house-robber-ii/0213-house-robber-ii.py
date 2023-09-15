35a4b596d6d6c5ad6a1b4f04d4156578306f631b
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-16 02:15:28 +0530

    Time: 34 ms (90.27%), Space: 16.3 MB (39.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,13 +1,23 @@
+#https://www.youtube.com/watch?v=rWAJCfYYOvM
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # Start by comparing two cases:
+        # 1. Rob the first house and consider the remaining houses from index 2 onward.
+        # 2. Skip the first house and consider all the houses from index 1 onward.
+        # The maximum of these two cases will be the result.
         return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
 
-    def helper(self,nums):
-        rob1 = 0
-        rob2 = 0
+    def helper(self, nums: List[int]) -> int:
+        rob1 = 0  # Maximum amount robbed from the previous house
+        rob2 = 0  # Maximum amount robbed from the house before the previous house
+
         for n in nums:
-            newRob = max(n+rob1,rob2)
-            rob1=rob2
-            rob2=newRob
+            # Calculate the maximum amount that can be robbed at the current house:
+            newRob = max(n + rob1, rob2)
+
+            # Update rob1 and rob2 for the next iteration:
+            rob1 = rob2
+            rob2 = newRob
+
+        # The final value of rob2 will represent the maximum amount that can be robbed.
         return rob2
-        
