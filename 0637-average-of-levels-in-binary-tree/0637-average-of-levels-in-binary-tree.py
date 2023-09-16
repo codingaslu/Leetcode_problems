2a8fe32175caeace2113aa8774a819a4c624996b
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-16 16:21:46 +0530

    Time: 51 ms (74.72%), Space: 18.9 MB (65.9%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -4,33 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        # Initialize an empty list to store the averages of each level.
+        averages = []
+
+        # Check if the root node is None (empty tree), and return an empty list if so.
         if not root:
             return []
-        res=[]
-        avg=0
-        from collections import deque
 
+        # Initialize a queue with the root node.
         queue = deque([root])
 
+        # Perform a level-order traversal of the binary tree.
         while queue:
-            level_sum=0
+            level_sum = 0
             level_count = len(queue)
-            for _ in range(len(queue)):
+
+            # Iterate through all nodes at the current level.
+            for _ in range(level_count):
                 node = queue.popleft()
-                level_sum+=node.val
+                level_sum += node.val
+
+                # Add the left and right children to the queue if they exist.
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            avg = level_sum / level_count  
-            res.append(avg) 
-        return res
-                
 
+            # Calculate the average value for the current level and append it to the result list.
+            averages.append(level_sum / level_count)
 
-
-
-
-        
+        # Return the list of level averages.
+        return averages
