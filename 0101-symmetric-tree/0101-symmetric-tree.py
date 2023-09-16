e8897eabbfe34d69969994ef5df7c1685e7ceab1
MUHAMMED ASLAM A <77477585+codingaslu@users.noreply.github.com>
2023-09-16 18:55:24 +0530

    Time: 39 ms (76.14%), Space: 16.4 MB (42.6%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,46 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # Check if the root is None, and if so, it's symmetric.
         if not root:
             return True
         
-
         from collections import deque
 
+        # Create a queue to perform a level-order traversal.
         queue = deque()
         queue.append(root.left)
         queue.append(root.right)
 
         while queue:
-                left = queue.popleft()  
-                right = queue.popleft()
+            # Pop the left and right nodes from the queue for comparison.
+            left = queue.popleft()  
+            right = queue.popleft()
 
-                if left ==  None and right == None:
-                    continue
+            # If both nodes are None, continue to the next pair.
+            if left ==  None and right == None:
+                continue
                 
-                if left == None and right != None:
-                    return False
+            # If one node is None and the other is not, the tree is not symmetric.
+            if left == None and right != None:
+                return False
                 
-                if left != None and right == None:
-                    return False                
+            if left != None and right == None:
+                return False                
 
-                if left.val != right.val:
-                    return False
+            # If the values of left and right nodes are not the same, the tree is not symmetric.
+            if left.val != right.val:
+                return False
 
-                queue.append(left.left)
-                queue.append(right.right)
-                queue.append(left.right)
-                queue.append(right.left)
+            # Add the left and right children of the current nodes to the queue for further comparison.
+            queue.append(left.left)
+            queue.append(right.right)
+            queue.append(left.right)
+            queue.append(right.left)
 
+        # If the loop completes without finding asymmetry, the tree is symmetric.
         return True
-
-
-
-
-
-
-
-
-        
